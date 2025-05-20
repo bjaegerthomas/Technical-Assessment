22038cb6fbcdb05cf3fcb78f32ebf431e2667bfb
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-20 14:33:23 -0400

    added js script for validating form entry for contact section of splash page

---
+++ Module 1/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form")
+const submitBtn = document.getElementById("submit");
+
+function submitForm() {
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    if (name.value.trim() === "" || email.value.trim() === "") {
+        document.getElementById("error").innerText = "Please enter your name and email address so we can follow up with you. Fields cannot be blank.";
+        return;
+}
+
+signInBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    submitForm();
+});}
