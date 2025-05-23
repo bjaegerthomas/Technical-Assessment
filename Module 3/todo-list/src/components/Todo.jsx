17390aead351dcbb4811388596e5e94f3aae3e6b
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-23 10:15:23 -0400

    refining addTodo function

--- Module 3/todo-list/src/components/Todo.jsx
+++ Module 3/todo-list/src/components/Todo.jsx
@@ -3,7 +3,20 @@ import { useState } from 'react';
 export default function Counter() {
     let [todoList, setTodoList] = useState('');
     let [inputTodo, setInputTodo] = useState('');
+    const [id, setId] = useState(0);
 
-    
+    const addTodo = () => {
+
+        if (inputTodo.trim() === '') {
+            alert('You didn\'t enter a todo');
+            return;
+        }
+
+        setTodoList([...todoList, {
+            id: id, text: inputTodo, completed: false
+        }]);
+            
+        setInputTodo('');
+    };
 
 }
