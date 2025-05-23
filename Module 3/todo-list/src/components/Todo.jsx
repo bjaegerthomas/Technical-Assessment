0298aa54c93188531131909d62a9d2e1b85e2ff9
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-23 10:01:58 -0400

    starting main code for todo.jsx

---
+++ Module 3/todo-list/src/components/Todo.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function Counter() {
+    let [todoList, setTodoList] = useState('');
+    let [inputTodo, setInputTodo] = useState('');
+
+    
+
+}
