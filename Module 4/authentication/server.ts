391eb08321c6e46d646d5af1adcaa12860a93618
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-27 12:21:12 -0400

    added server code, augmented from class code

---
+++ Module 4/authentication/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import db from './config/connection.js'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './utils/auth.js';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
+
+startApolloServer();
