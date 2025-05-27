e3ac3ae92f62a34e86451f406ce1917dbf0f7ce0
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-27 10:36:14 -0400

    updated auth.ts

---
+++ Module 4/authentication/utils/auth.ts
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import { GraphQLError } from 'graphql';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const authenticateToken = ({ req }: any) => {
+  let token = req.body.token || req.query.token || req.headers.authorization;
+
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
+
+  if (!token) {
+    return req;
+  }
+
+  try {
+    const { data }: any = jwt.verify(token, process.env.JWT_SECRET_KEY || '');
+
+    req.user = data;
+  } catch (err) {
+    console.log('Invalid token');
+  }
+
+  return req;
+};
+
+export const signToken = (email: string, _id: unknown) => {
+
+  const payload = { email, _id };
+  const secretKey: any = process.env.JWT_SECRET_KEY;
+
+  return jwt.sign({ data: payload }, secretKey, { expiresIn: '2h' });
+};
+
+export class AuthenticationError extends GraphQLError {
+  constructor(message: string) {
+    super(message, undefined, undefined, ['UNAUTHENTICATED']);
+    Object.defineProperty(this, 'email', { value: 'AuthenticationError' });
+  }
+};
