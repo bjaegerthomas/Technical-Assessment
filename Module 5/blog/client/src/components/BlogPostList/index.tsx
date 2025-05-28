94e5cf747dd19cb375c8ce8a2c664d6022b3b16c
jaegerthomas <bjaegerthomas@gmail.com>
2025-05-28 18:57:10 -0400

    added blogpostlist

---
+++ Module 5/blog/client/src/components/BlogPostList/index.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+interface BlogPost {
+  _id: string;
+  title: string;
+  author: string;
+  content: string;
+  createdAt: string;
+}
+
+interface BlogPostListProps {
+  blogPosts: BlogPost[];
+  title: string;
+}
+
+const BlogPostList: React.FC<BlogPostListProps> = ({ blogPosts, title }) => {
+  if (!blogPosts.length) {
+    return <h3>No Thoughts Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {blogPosts &&
+        blogPosts.map((blogPost) => (
+          <div key={blogPost._id}>
+            <h4>
+              {blogPost.title} <br />
+            </h4>
+            <div>
+              <p>{blogPost.author}</p>
+            </div>
+            <Link
+              to={`/blogPosts/${blogPost._id}`}
+            >
+              Check Out this BlogPost.
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default BlogPostList;
